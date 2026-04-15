cac06fbe1e9e97f03f2c9e6348ee9609d2cda9a9
EminGasimli <gasimliemin@gmail.com>
2026-04-15 20:41:37 +0400

    Initial commit

---
+++ src/Lesson1/Main.java
@@ -0,0 +1,12 @@
+package Lesson1;
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Salam dunya");
+        System.out.println("Emin");
+        System.out.println("Eli");
+        System.out.println("Yusif");
+        System.out.println("Rustem");
+    }
+}

fcb8a5012dfdc6472de401b60acf026f5df7b3d3
EminGasimli <gasimliemin@gmail.com>
2026-04-22 19:33:57 +0400

    commit Lesson3

---
+++ src/Lesson3/Main.java
@@ -0,0 +1,23 @@
+package Lesson3;
+
+public class Main {
+    static void main() {
+        byte a = 127;
+        short b = 2323;
+        int c = 123456;
+        double d = 3.12392;
+        float e = 577F;
+        long f = 438749384732L;
+        char h = 'A';
+        boolean check = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(h);
+        System.out.println(check);
+    }
+}

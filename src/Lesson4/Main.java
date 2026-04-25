9b761681764385c050e2a67de3aad2b4734cc289
EminGasimli <gasimliemin@gmail.com>
2026-04-25 11:59:46 +0400

    commit Lesson4

---
+++ src/Lesson4/Main.java
@@ -0,0 +1,22 @@
+package Lesson4;
+
+public class Main {
+    static void main() {
+        int a,b,c;
+        a = 120;
+        b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        c = 2367;
+        if (c % 2 == 0) {
+            System.out.println("Eded Cutdur");
+        }
+        else{
+            System.out.println("Eded Tekdir");
+        }
+    }
+}

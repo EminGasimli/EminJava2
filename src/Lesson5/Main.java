e074db0c7578ce0dc6217a3796f1f0d222413d9d
EminGasimli <gasimliemin@gmail.com>
2026-04-28 19:03:09 +0400

    commit Lesson5

---
+++ src/Lesson5/Main.java
@@ -0,0 +1,24 @@
+package Lesson5;
+
+public class Main {
+    static void main() {
+        int age = 7;
+        System.out.println(age);
+        age = 14;
+        byte b = 7;
+        short s = 7;
+        long l = 7;
+        double d = 7.9D;
+        float f = 7.9F;
+        char c = 'A';
+        boolean hasHome = true;
+        System.out.println(age);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(hasHome);
+    }
+}

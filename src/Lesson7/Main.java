2d349c69cdecca049b28153794c717aa49a03303
EminGasimli <gasimliemin@gmail.com>
2026-05-02 16:55:39 +0400

    commit Lesson7

---
+++ src/Lesson7/Main.java
@@ -0,0 +1,10 @@
+package Lesson7;
+
+public class Main {
+    static void main() {
+        char a = 'A';
+        boolean test = true;
+        System.out.println(a);
+        System.out.println(test);
+    }
+}

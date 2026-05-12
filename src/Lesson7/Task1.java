b385329329ec15d27a8ee3a7482e35edf53675fb
EminGasimli <gasimliemin@gmail.com>
2026-05-12 21:47:28 +0400

    commit Lesson7

---
+++ src/Lesson7/Task1.java
@@ -0,0 +1,13 @@
+package Lesson7;
+
+public class Task1 {
+    public static void main() {
+        int bal1 = 80;
+        int bal2 = 85;
+        int bal3 = 90;
+
+        double ortalama = (bal1 + bal2 + bal3) / 3.0;
+
+        System.out.println("Ortalama: " + ortalama);
+    }
+}
---
+++ src/Lesson7/Task2.java
@@ -0,0 +1,9 @@
+package Lesson7;
+
+public class Task2 {
+    public static void main() {
+        double balans = 150.75;
+        int nagdPul = (int) balans;
+        System.out.println(nagdPul);
+    }
+}

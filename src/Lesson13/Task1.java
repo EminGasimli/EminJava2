62300cb5ae4e22528a2de10b5d88977a28f691f2
EminGasimli <gasimliemin@gmail.com>
2026-05-26 19:30:47 +0400

    commit Lesson11

---
+++ src/Lesson13/Task1.java
@@ -0,0 +1,14 @@
+package Lesson13;
+
+public class Task1 {
+    static void main() {
+        int age = 45;
+        boolean isStudent = true;
+        if (age > 65 || isStudent == true){
+            System.out.println("Endirim qazandiniz.");
+        }
+        else{
+            System.out.println("Endirim yoxdur.");
+        }
+    }
+}
